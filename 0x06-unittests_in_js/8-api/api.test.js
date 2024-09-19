960db46b81785a0627446d6557dd45dc9ca6d1af
medriadi <mohamedriadi@outlook.com>
2024-09-19 12:57:20 +0100

    feat: Add .gitignore file to ignore node_modules

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,26 @@
 const request = require('request');
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const app = require('./api');  // Ensure the server is loaded for the test
 
-describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
 
-  it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.headers['content-type']).to.contain('text/html');
       done();
     });
   });
